93485ec2a3d67f5c4b49489c09d9f1aaab6cd882
matheus <teste@gemail>
2022-08-18 22:10:46 -0300

    feat: atividades aula 01 e 02

---
+++ aula02/exercicios/CalculoMedia/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Ex1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double[] notas = {2,4,6,8,2, 3, 3, 3};
+      double media = CalcularMedia(notas);
+      double mediana = CalcularMediana(notas);
+      double moda = CalcularModa(notas);
+      Console.WriteLine($"A media de notas é: {media}");
+      Console.WriteLine($"A mediana de notas é: {mediana}");
+      Console.WriteLine($"A moda de notas é: {moda}");
+    }
+
+    static double CalcularMedia(double[] notas)
+    {
+      double media = 0.0;
+      double soma = 0.0;
+      for (int i = 0; i < notas.Length; i++)
+      {
+        soma += notas[i];
+      }
+      media = soma / notas.Length;
+      return media;
+    }
+
+    static double CalcularMediana (double[] notas)
+    {
+      double mediana = 0;
+      Array.Sort(notas); 
+      if (notas.Length % 2 == 0)
+      {       
+        mediana = (notas[notas.Length / 2] + notas[notas.Length / 2 - 1]) / 2;
+      }
+      else
+      {
+        mediana = notas[notas.Length / 2];
+      }
+      return mediana;
+    }
+
+    static double CalcularModa(double[] notas)
+    {
+      double moda = 0;
+      int[] vetor = new int[notas.Length];
+      for (int i = 0; i < notas.Length; i++)
+      {
+        for (int j = 0; j < notas.Length; j++)
+        {
+          if (notas[i] == notas[j])
+          {
+            vetor[i]++;
+          }
+        }
+      }
+      
+      int maior = 0;
+      for (int i = 0; i < vetor.Length; i++)
+      {
+        if (vetor[i] > maior)
+        {
+          maior = vetor[i];
+          moda = notas[i];
+        }
+      }
+      return moda;
+    }
+  }
+}
+
+
---
+++ aula02/exercicios/ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex1 {
+  class Program {
+    static void Main (string[] args){
+      Console.WriteLine("--- conversor de moedas ---");
+      Console.WriteLine("Digite o valor em real: ");
+      double real = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Digite a moeda para converter: ");
+      string moeda = Console.ReadLine();
+      double conversao = 0;
+      switch (moeda) {
+        case "dolar":
+          conversao = real / 5.17;
+          break;
+        case "euro":
+          conversao = real / 6.14;
+          break;
+        case "peso argentino":
+          conversao = real / 0.05;
+          break;
+        case "yen":
+          conversao = real / 0.28;
+          break;
+        default:
+          Console.WriteLine("Moeda inválida");
+          break;
+      }
+      Console.WriteLine($"{real} reais equivalem a {conversao} {moeda}");
+      Console.Write($"{Environment.NewLine}Presione qualquer tecla para sair...");
+    }
+  }
+}
+
